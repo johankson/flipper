70131a30396fe5fe1f840a249385291601eabde7
Daniel Hindrikes <daniel@hindrikes.se>
2015-04-27 08:30:45 -0700

    created windows phone project and empty renderer

---
+++ Flipper.WinPhone/Renderers/SwiperRenderer.cs
@@ -0,0 +1,16 @@
+﻿using Flipper.Controls;
+using Flipper.WinPhone.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(Swiper), typeof(SwiperRenderer))]
+namespace Flipper.WinPhone.Renderers
+{
+    public class SwiperRenderer
+    {
+    }
+}
