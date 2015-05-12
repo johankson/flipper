f160ff02b62f6dadd861fd0d5b54893bc962782e
Daniel Hindrikes <daniel@hindrikes.se>
2015-05-12 10:56:40 +0200

    card content view

--- Flipper.Sample/Flipper.Sample/LinkerBeGone.cs
+++ Flipper.Sample/Flipper.Sample/LinkerBeGone.cs
@@ -8,6 +8,7 @@ namespace Flipper.Sample
         {
             // This is needed to ensure that the linker doesn't remove the control
             var a = new Swiper();
+            var b = new CardContentView();
         }
     }
 }
---
+++ Flipper/Controls/CardContentView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Flipper.Controls
+{
+        public class CardContentView : ContentView
+        {
+            public static readonly BindableProperty CornerRadiusProperty =
+                BindableProperty.Create<CardContentView, float>
+            (p => p.CornderRadius, 3.0F);
+
+            public float CornderRadius
+            {
+                get { return (float)GetValue(CornerRadiusProperty); }
+                set { SetValue(CornerRadiusProperty, value); }
+            }
+
+            protected override SizeRequest OnSizeRequest(double widthConstraint, double heightConstraint)
+            {
+                if (Content == null)
+                    return new SizeRequest(new Size(100, 100));
+
+                return Content.GetSizeRequest(widthConstraint, heightConstraint);
+            }
+
+            public CardContentView()
+            {
+                Padding = 20;
+            }
+        }
+}
