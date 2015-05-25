a5a2d3d97348df76b38eea034454d240d0d75da3
johan.kson@gmail.com <johan.kson@gmail.com>
2015-05-25 13:32:54 +0200

    Fixed faulty gitignore file and added missing files

---
+++ Flipper.Sample/Flipper.Sample/ViewModels/SwiperViewModel.cs
@@ -0,0 +1,112 @@
+﻿using Flipper.Sample.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Flipper.Sample.ViewModels
+{
+    class SwiperViewModel : INotifyPropertyChanged
+    {
+        ObservableCollection<string> _items;
+        public ObservableCollection<string> Items
+        {
+            get { return _items; }
+            set { _items = value; NotifyPropertyChanged("Items"); }
+        }
+
+        int _itemCount;
+        public int ItemCount
+        {
+            get { return _itemCount; }
+            set { _itemCount = value; NotifyPropertyChanged("ItemCount"); }
+        }
+
+        int _index;
+        public int Index
+        {
+            get { return _index; }
+            set { 
+                _index = value; 
+                NotifyPropertyChanged("Index");
+                Status = String.Format("{0} of {1}", _index + 1, this.Items.Count);
+            }
+        }
+
+        string _url;
+        public string Url
+        {
+            get { return _url; }
+            set
+            {
+                _url = value;
+                NotifyPropertyChanged("Url");
+                Status = String.Format("{0} of {1}", _index + 1, this.Items.Count);
+            }
+        }
+
+        string _status;
+        public string Status
+        {
+            get { return _status; }
+            set { _status = value; NotifyPropertyChanged("Status"); }
+        }
+
+        public ICommand EndIsNearCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    // The end is near, add more random images
+                    SwiperView.AddFiveRandomImages(Items);
+                });
+            }
+        }
+
+        public ICommand GotoRandomIndex
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    var r = new Random();
+                    Index = r.Next(0, Items.Count - 1);
+                });
+            }
+        }
+
+        public ICommand GotoRandomUrl
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    var r = new Random();
+                    var randomIndex = r.Next(0, Items.Count - 1);
+                    var randomUrl = Items[randomIndex];
+                    Url = randomUrl;
+                });
+            }
+        }
+        
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Flipper.Sample/Flipper.Sample/Views/SwiperView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Flipper.Sample.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Flipper.Sample.Views
+{
+    public partial class SwiperView : ContentPage
+    {
+        public SwiperView()
+        {
+            InitializeComponent();
+            BindingContext = CreateViewModel();
+        }
+
+        private static SwiperViewModel CreateViewModel()
+        {
+            var items = new ObservableCollection<string>();
+
+            // Create dummy data by randomly adding 5 images of different sizes
+            AddFiveRandomImages(items);
+
+            // Select the third image
+           // var third = items.Skip(2).First();
+
+            // At the momement we need to match both index and url
+            var model = new SwiperViewModel()
+            {
+                Items = items,
+                Index = 2
+            };
+            return model;
+        }
+
+        public static void AddFiveRandomImages(ObservableCollection<string> items)
+        {
+            // We need the guid to create a unique URL to ensure
+            // that no caching is going on!
+            var key = Guid.NewGuid().ToString();
+
+            var r = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+             //   var url = String.Format("http://dummyimage.com/{0}x{1}/fff?a={2}",
+             //                           r.Next(100, 1400), r.Next(100, 1000), key);
+                var url = String.Format("http://lorempixel.com/{0}/{1}/?a={2}",
+                                       r.Next(100, 1400), r.Next(100, 1000), key);
+            
+                if (!items.Contains(url))
+                {
+                    items.Add(url);
+                }
+            }
+        }
+    }
+}
