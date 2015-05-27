9a852c5987f80a1f49e5b41be8167c85fdb54d5d
johan.kson@gmail.com <johan.kson@gmail.com>
2015-05-27 14:45:02 +0200

    Added a LinkerBeGone file to the WP project

---
+++ Flipper.Sample/Flipper.Sample.WinPhone/LinkerBeGone.cs
@@ -0,0 +1,20 @@
+﻿using Flipper.WinPhone.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flipper.Sample.WinPhone
+{
+    class LinkerBeGone
+    {
+        /// <summary>
+        /// We simply need to create a reference to the type to get it loaded
+        /// </summary>
+        public LinkerBeGone()
+        {
+            var a = new SwiperRenderer();
+        }
+    }
+}
